85ad8361d32e2a2ea5ab911c3b6aea117fb4c867
占峰 <zhanfeng@zhanfengdemacbook-pro.local>
2017-06-07 15:57:41 +0800

    Initial Commit

---
+++ CommonStaticTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommonStaticTableView
+//
+//  Created by zhanfeng on 2017/6/7.
+//  Copyright © 2017年 lzf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CommonStaticTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommonStaticTableView
+//
+//  Created by zhanfeng on 2017/6/7.
+//  Copyright © 2017年 lzf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
